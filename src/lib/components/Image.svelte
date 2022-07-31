27ca33037f3baede1e989744db6ae6639c69b563
imperatrona <xb2w1z@gmail.com>
2022-07-31 20:10:08 +0300

    image optimization with vite-imagetools

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let image;
+	export let alt;
+
+	const srcset = image.map(media => {
+		if (!media.cropped) {
+			return `${media.src} ${media.width}w`
+		}
+	})
+</script>
+<figure>
+<img
+	sizes="(max-width: 600px) 540px, (max-width: 1400px) 1280px,
+	            2048px"
+	srcset={srcset}
+	src={image[image.length - 1].src}
+	height={image[image.length - 1].height}
+	width={image[image.length - 1].width}
+	loading="lazy"
+	alt={alt}>
+</figure>
