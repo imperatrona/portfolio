9285441ec3b7d936e110d95d7f41618e5f06190a
imperatrona <xb2w1z@gmail.com>
2022-09-17 14:27:27 +0300

    updated translations

--- src/lib/translations/index.ts
+++ src/lib/translations/index.ts
@@ -3,46 +3,34 @@ import lang from './lang.json';
 
 /** @type {import('sveltekit-i18n').Config} */
 const config = {
-  translations: {
-    en: { lang },
-    ru: { lang }
-  },
-  loaders: [
-    {
-      locale: 'en',
-      key: 'common',
-      loader: async () => (await import('./en/common.json')).default
-    },
-    {
-      locale: 'en',
-      key: 'home',
-      routes: ['/'],
-      loader: async () => (await import('./en/home.json')).default
-    },
-    {
-      locale: 'en',
-      key: 'about',
-      routes: ['/about'],
-      loader: async () => (await import('./en/about.json')).default
-    },
-    {
-      locale: 'ru',
-      key: 'common',
-      loader: async () => (await import('./ru/common.json')).default
-    },
-    {
-      locale: 'ru',
-      key: 'home',
-      routes: ['/'],
-      loader: async () => (await import('./ru/home.json')).default
-    },
-    {
-      locale: 'ru',
-      key: 'about',
-      routes: ['/about'],
-      loader: async () => (await import('./ru/about.json')).default
-    },
-  ]
+	translations: {
+		en: { lang },
+		ru: { lang }
+	},
+	loaders: [
+		{
+			locale: 'en',
+			key: 'common',
+			loader: async () => (await import('./en/common.json')).default
+		},
+		{
+			locale: 'en',
+			key: 'home',
+			routes: ['/'],
+			loader: async () => (await import('./en/home.json')).default
+		},
+		{
+			locale: 'ru',
+			key: 'common',
+			loader: async () => (await import('./ru/common.json')).default
+		},
+		{
+			locale: 'ru',
+			key: 'home',
+			routes: ['/'],
+			loader: async () => (await import('./ru/home.json')).default
+		}
+	]
 };
 
 export const defaultLocale = 'en';
