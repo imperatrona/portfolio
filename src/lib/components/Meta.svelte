2d10d8c4bcf9c9ecf18f437af74c47838846215a
imperatrona <xb2w1z@gmail.com>
2022-09-17 12:22:27 +0300

    update meta component

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -1,31 +1,32 @@
 <script>
 	export let date;
 	export let tags = [];
-
-	const formattedDate = new Date(date).toLocaleDateString('en-US', {
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric'
-	});
 </script>
 
 <div class="meta">
-	<span class="date">{formattedDate}</span>
-	{#if tags.length !== 0}
+	<span class="date"
+		>{new Date(date).toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		})}</span
+	>
+	{#if tags !== undefined && tags.length !== 0}
+		{` · `}
 		<div class="tags">
 			{#each tags as tag}
-				<a href="/tags/{tag}">{tag}</a>
+				<a href="/blog/tag/{tag}">{tag}</a>
 			{/each}
 		</div>
 	{/if}
 </div>
 
 <style>
-	.date {
-		color: rgba(var(--color-text-primary), 0.55);
-	}
+	/*.date {
+	}*/
 
 	.meta {
+		color: rgba(var(--color-text-primary), 0.55);
 		font-size: 0.8em;
 		display: flex;
 		gap: 8px;
@@ -37,6 +38,8 @@
 	.tags a {
 		text-decoration: underline solid rgba(var(--color-text-primary), 0.2);
 		text-underline-offset: 6px;
+
+		text-decoration: none;
 	}
 	.tags a:hover {
 		text-decoration: none;
