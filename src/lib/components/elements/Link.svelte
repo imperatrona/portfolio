e4c75c05bf579379fbceb2fb0fe553fac506a73a
imperatrona <xb2w1z@gmail.com>
2022-09-17 12:21:25 +0300

    link for language routes

---
+++ src/lib/components/elements/Link.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { locale, defaultLocale } from '$lib/translations';
+
+	export let href: string;
+</script>
+
+<a href={`${$locale == defaultLocale ? '' + href : '/' + $locale + (href == '/' ? '' : href)}`}
+	><slot /></a
+>
