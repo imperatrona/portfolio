4765ce72cf4fa271a635d9aa55386ef9cea69453
imperatrona <xb2w1z@gmail.com>
2022-11-15 20:19:37 +0300

    updated selected row

--- src/routes/[...lang=lang]/+page.svelte
+++ src/routes/[...lang=lang]/+page.svelte
@@ -6,16 +6,15 @@
 	import Horny from '$lib/components/projects/Horny.svelte';
 	import Spacer from '$lib/components/Spacer.svelte';
 	import Hero from '$lib/components/Hero.svelte';
+	import SelectedHeader from '$lib/components/SelectedHeader.svelte';
 </script>
 
 <PageHead title={$t('home.title')} description="An awesome blog about development with Svelte" />
 
 <div>
-	<div class="container">
-		<p class="meta">{$t('common.selected.projects')}</p>
+	<Hero />
 	<Spacer />
-	</div>
-	<TwitterWidget />
+	<SelectedHeader />
 	<div class="projects">
 		<Horny />
 		<TwitterWidget />
@@ -23,25 +22,9 @@
 </div>
 
 <style>
-	h1 {
-		font-size: calc(7vw + 1vh + 2vmin);
-		line-height: 1.2;
-		font-weight: 350;
-		letter-spacing: -0.05em;
-		text-align: left;
-		margin: 0;
 	.projects {
 		display: flex;
 		flex-direction: column;
 		gap: 10px;
-	}
-	}
-
-	}
-
-	@media (min-width: 850px) {
-		}
-		}
-		}
 	}
 </style>
