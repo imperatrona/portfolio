fdd8058ce46db25836208d459e6e150faf784346
imperatrona <xb2w1z@gmail.com>
2022-11-15 19:56:49 +0300

    multi fromat support

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -2,24 +2,46 @@
 	export let image;
 	export let alt;
 
-	const srcset = image.map(media => {
+	let data = {};
+
+	image.forEach((media) => {
 		if (!media.cropped) {
-			return `${media.src} ${media.width}w`
+			if (!data[media.format]) {
+				data[media.format] = {
+					srcset: [],
+					nodes: []
+				};
+			}
+
+			data[media.format].nodes.push(media);
+			data[media.format].srcset.push(`${media.src} ${media.width}w`);
 		}
-	})
+	});
 </script>
+
 <figure>
-<img
-	sizes="(max-width: 600px) 540px, (max-width: 1400px) 1280px,
+	<picture>
+		{#each Object.keys(data) as format}
+			{#if Object.keys(data)[Object.keys(data).length - 1] == format}
+				<img
+					sizes="(max-width: 600px) 540px, (max-width: 1400px) 1280px,
 	            2048px"
-	srcset={srcset}
-	src={image[image.length - 1].src}
-	height={image[image.length - 1].height}
-	width={image[image.length - 1].width}
-	loading="lazy"
-	alt={alt}>
+					srcset={data[format].srcset}
+					src={data[format].nodes[data[format].nodes.length - 1].src}
+					height={data[format].nodes[data[format].nodes.length - 1].height}
+					width={data[format].nodes[data[format].nodes.length - 1].width}
+					loading="lazy"
+					{alt}
+				/>
+			{:else}
+				<source type={`image/${format}`} srcset={data[format].srcset} />
+			{/if}
+		{/each}
+	</picture>
 </figure>
 
 <style>
-	img{display: block;}
+	img {
+		display: block;
+	}
 </style>
